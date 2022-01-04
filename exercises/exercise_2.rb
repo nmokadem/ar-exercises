7a00a378d9903e02dc8fcbcfca912e672b4ee0c2
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-04 19:52:20 +0000

    Delete third record

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,25 +5,7 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
-
-class Exercise_2
-  attr_accessor :store1, :store2
-
-  def initialize
-    #@store1 = Store.first
-    @store1 = Store.find_by(id: 1)
-    @store2 = Store.where('id > 1').limit(1).order('id asc')
-    puts @store1
-  end
-
-  def update
-    @store1.name = 'English Bay'
-    @store1.save
-  end
-
-end
-
-s = Exercise_2.new.update
-
-puts "#{s.store1}"
-puts "#{s.store2}"
+@store1 = Store.find_by(id: 1)
+@store2 = Store.where('id > 1').limit(1).order('id asc')
+@store1.name = 'English Bay'
+@store1.save
