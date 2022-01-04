833a43534fb48587a96d5a23d106e6aa345f5f57
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-04 23:50:56 +0000

    Add employees records

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,6 +6,7 @@ puts "----------"
 
 # Your code goes here ...
 @store1 = Store.find_by(id: 1)
-@store2 = Store.where('id > 1').limit(1).order('id asc')
+#@store2 = Store.where('id > 1').limit(1).order('id asc')
+@store2 = Store.find_by(id: 2)
 @store1.name = 'English Bay'
 @store1.save
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -16,9 +16,3 @@ puts average_revenue
 
 count_stores = Store.where("annual_revenue > 1000000").count
 puts count_stores 
-
-# 1. Output the total revenue for the entire company (all stores), using Active 
-# Record's `.sum` calculation method.
-# 2. On the next line, also output the average annual revenue for all stores.
-# 3. Output the number of stores that are generating $1M or more in annual sales. 
-# **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
