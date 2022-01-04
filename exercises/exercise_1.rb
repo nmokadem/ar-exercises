62c1d901d0a76c15c5f531050378b0c91f94a33c
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-04 02:35:47 +0000

    Create 3 records in stores relation

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,19 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+def add_record(name,revenue,men_apparel,women_apparel)
+  store = Store.new do |s|
+    s.name = name
+    s.annual_revenue = revenue
+    s.mens_apparel = men_apparel
+    s.womens_apparel = women_apparel
+  end
+
+  store.save
+end
+
+add_record("Burnaby", 300000, true, true)
+add_record("Richmond", 126000, false, true)
+add_record("Burnaby", 190000, true, false)
+
+puts "# of records in Store is #{Store.all.count} records."
