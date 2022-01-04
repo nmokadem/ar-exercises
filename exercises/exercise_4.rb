b33f34f71c2f9a92e3ff35543829f14e04a2f9d6
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-04 22:08:53 +0000

    Execute queries for exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,12 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+add_record("Surrey",    224000, false, true)
+add_record("Whistler", 1900000, true, false)
+add_record("Yaletown",  430000, true, true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each { |s| puts "#{s.name} #{s.annual_revenue}" }
+
+@womens_stores =  Store.where("womens_apparel ? AND annual_revenue < ?",true,  1000000)
